284c69a2c3dc19e47f6b7d7b208ba9a52cb2e2ce
Vu_Duc_Khanh <dk.assassins.9x@gmail.com>
2023-12-26 23:45:40 +0700

    topKFrequent_python

--- python/topKFrequent.py
+++ python/topKFrequent.py
@@ -1,3 +1,21 @@
+from typing import List
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        
+        count = {}
+        freq = [[] for i in (range(len(nums)+1))]
+        for n in nums:
+            count[n] = 1 + count.get(n, 0);
+        for n, c in count.items():
+            freq[c].append(n)
+        result = []
+        for i in range(len(freq) - 1, 0, -1):
+            for n in freq[i]:
+                result.append(n)
+            if (len(result) == k):
+                return result
+            
+if __name__ == "__main__":
+    nums = [1,1,1,2,2,3]
+    k = 2
+    result = Solution().topKFrequent(nums, k)
+    print(result)
