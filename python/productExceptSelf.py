e5e6a8c69bc0e40b5c6db4f2be261f2ad7fe0d15
Vu_Duc_Khanh <dk.assassins.9x@gmail.com>
2023-12-27 00:23:32 +0700

    productExceptSelf

---
+++ python/productExceptSelf.py
@@ -0,0 +1,17 @@
+from typing import List
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        result = [1] * len(nums)
+        for i in range(1, len(nums)):
+            result[i] = result[i-1] * nums[i-1]
+        right = 1
+        for i in range(len(nums)-2, -1, -1):
+            right = right * nums[i+1]
+            result[i] *= right
+        return result
+    
+if __name__ == "__main__":
+    nums = [1,2,3,4]
+    result = Solution().productExceptSelf(nums)
+    print(result)
